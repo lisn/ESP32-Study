7c197307aeaa9fd73930e376e98ec8ecd128af45
shenlei <shenl@xydtek.com>
2026-02-26 18:33:25 +0800

    在任务里面，删除自身，参数为NULL.

--- code/01_get-started/task/main/task_main.c
+++ code/01_get-started/task/main/task_main.c
@@ -13,6 +13,7 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
+#if 0
 void myTask(void *pvParam)
 {
 	while (1)
@@ -39,3 +40,17 @@ void app_main(void)
 		printf("Failed to create myTask.\n");
 	}
 }
+#else
+void myTask(void *pvParam)
+{
+
+	printf("This is my first task! I am happy.\n");
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+	vTaskDelete(NULL);
+}
+void app_main(void)
+{
+
+	xTaskCreate(myTask, "myTask01", 1024, NULL, 1, NULL);
+}
+#endif
