d8d9afc030aeb3115f2976340a0244cb5dfee0d8
shenlei <shenl@xydtek.com>
2026-02-28 15:52:51 +0800

    task stack:如何调试分配任务栈大小。

---
+++ code/01_get-started/task_stack/main/task_stack_main.c
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+#if 1
+
+void myTask1(void *pvParam)
+{
+
+	while (1)
+	{
+		printf("Task 1 is running\n");
+		// printf("\n");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+	TaskHandle_t pxTask1 = NULL;
+
+	// xTaskCreate(myTask1, "myTask01", 2048, NULL, 1, &pxTask1);
+	xTaskCreate(myTask1, "myTask01", 512, NULL, 1, &pxTask1);
+	//if the stack size is too small, the task will run abnormally, and the stack high water mark will be 0
+	// xTaskCreate(myTask1, "myTask01", 256, NULL, 1, &pxTask1);
+
+	UBaseType_t iStack = uxTaskGetStackHighWaterMark(pxTask1);
+	while (1)
+	{
+		printf("Stack High Water Mark for Task 1: %d\n", iStack);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+	
+}
+#else
+
+#endif
