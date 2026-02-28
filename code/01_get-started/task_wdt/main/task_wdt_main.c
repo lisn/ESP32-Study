87ce2909814a5144d893d5f89d58676568e47ceb
shenlei <shenl@xydtek.com>
2026-02-28 17:27:05 +0800

    task wdt:修改任务的优先级与idle的优先级一样，idle有分配到时间可以运行。别一种方式，在任务中主动让度cpu权，比如调用vTaskDelay。

--- code/01_get-started/task_wdt/main/task_wdt_main.c
+++ code/01_get-started/task_wdt/main/task_wdt_main.c
@@ -13,7 +13,7 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
-#if 1
+#if 0
 
 void myTask1(void *pvParam)
 {
@@ -43,4 +43,30 @@ void app_main(void)
 }
 #else
 
+void myTask1(void *pvParam)
+{
+
+	while (1)
+	{
+		// printf("Task 1 is running\n");
+		//vTaskDelay(1000 / portTICK_PERIOD_MS);
+		;
+	}
+
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+	TaskHandle_t pxTask1 = NULL;
+	xTaskCreate(myTask1, "myTask01", 512, NULL, 0, &pxTask1);
+	
+	UBaseType_t iStack = uxTaskGetStackHighWaterMark(pxTask1);
+	while (1)
+	{
+		printf("Stack High Water Mark for Task 1: %d\n", iStack);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+	
+}
 #endif
