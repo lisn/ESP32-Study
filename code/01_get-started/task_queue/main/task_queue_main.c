7e4695d80c7ff8a52b5d920637332e36654f1df3
shenlei <shenl@xydtek.com>
2026-03-02 13:51:13 +0800

    task queue:传递结构体例子。

--- code/01_get-started/task_queue/main/task_queue_main.c
+++ code/01_get-started/task_queue/main/task_queue_main.c
@@ -71,7 +71,7 @@ void app_main(void)
 	xTaskCreate(recTask, "recTask", 2048, (void *)xQueue, 1, NULL);
 }
 
-#else
+#elif 0
 
 void sndTask(void *pvParam)
 {
@@ -127,6 +127,72 @@ void app_main(void)
 {
 	QueueHandle_t xQueue;
 	xQueue = xQueueCreate(10, sizeof(int));
+
+	xTaskCreate(sndTask, "sndTask", 2048, (void *)xQueue, 1, NULL);
+	xTaskCreate(recTask, "recTask", 2048, (void *)xQueue, 1, NULL);
+}
+#else
+typedef struct
+{
+	int id;
+	int data;
+} A_STRUCT ;
+
+void sndTask(void *pvParam)
+{
+	QueueHandle_t xQueue = (QueueHandle_t)pvParam;
+	A_STRUCT data = {0, 99};
+	while (1)
+	{
+		if (xQueueSend(xQueue, &data, 0) != pdTRUE)
+		{
+			printf("sndTask send data failed\n");
+		}
+		else
+		{
+			printf("sndTask send data: id=%d, data=%d\n", data.id, data.data);
+		}
+
+		data.id++;
+
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+
+	vTaskDelete(NULL);
+}
+
+void recTask(void *pvParam)
+{
+
+	QueueHandle_t xQueue = (QueueHandle_t)pvParam;
+
+	A_STRUCT data;
+	while (1)
+	{
+		int count = uxQueueMessagesWaiting(xQueue);
+		printf("recTask queue count: %d\n", count);
+
+		if (count > 0)
+		{
+			if (xQueueReceive(xQueue, &data, 0) == pdTRUE)
+			{
+				printf("recTask rcvd data: id=%d, data=%d\n", data.id, data.data);
+			}
+			else
+			{
+				printf("recTask receive data failed\n");
+			}
+		}
+
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+
+	vTaskDelete(NULL);
+}
+void app_main(void)
+{
+	QueueHandle_t xQueue;
+	xQueue = xQueueCreate(10, sizeof(A_STRUCT));
 
 	xTaskCreate(sndTask, "sndTask", 2048, (void *)xQueue, 1, NULL);
 	xTaskCreate(recTask, "recTask", 2048, (void *)xQueue, 1, NULL);
