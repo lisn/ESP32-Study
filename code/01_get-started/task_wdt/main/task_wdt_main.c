12ee7bf796c99481a34b1a190fcd413f84118eb0
shenlei <shenl@xydtek.com>
2026-03-02 10:57:40 +0800

    task wdt:监控一个任务。目的是监控譔任务运行的时间内，有没有清除看门狗；还有一点就是必须主动让度CPU。

--- code/01_get-started/task_wdt/main/task_wdt_main.c
+++ code/01_get-started/task_wdt/main/task_wdt_main.c
@@ -12,6 +12,8 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include "esp_task_wdt.h"
+#include "esp_log.h" 
 
 #if 0
 
@@ -41,7 +43,7 @@ void app_main(void)
 	}
 	
 }
-#else
+#elif 0
 
 void myTask1(void *pvParam)
 {
@@ -67,6 +69,35 @@ void app_main(void)
 		printf("Stack High Water Mark for Task 1: %d\n", iStack);
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
+	
+}
+#else
+
+static const char *TAG = "WDT_TEST";
+void myTask1(void *pvParam)
+{
+	esp_err_t ret = esp_task_wdt_add(NULL);
+    if (ret == ESP_OK) {
+         ESP_LOGI(TAG, "任务已添加到 WDT 监控");  // 现在可以正常使用了
+    } else {
+        ESP_LOGE(TAG, "添加到 WDT 失败，错误码: %d", ret);
+        vTaskDelete(NULL);
+        return;
+    }
+
+	while (1)
+	{
+		printf("Task 1 is running\n");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		esp_task_wdt_reset();
+	}
+
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+	xTaskCreate(myTask1, "myTask01", 2048, NULL, 1, NULL);
 	
 }
 #endif
