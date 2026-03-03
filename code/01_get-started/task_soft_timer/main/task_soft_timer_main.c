b5bdb2a48bbda2695233123c4966dc85994c1850
shenlei <shenl@xydtek.com>
2026-03-03 15:29:33 +0800

    timer:获得定时的名字与id.

--- code/01_get-started/task_soft_timer/main/task_soft_timer_main.c
+++ code/01_get-started/task_soft_timer/main/task_soft_timer_main.c
@@ -16,13 +16,19 @@
 
 void timer_callback(TimerHandle_t xTimer){
 		static uint32_t count = 0;
-		ESP_LOGI("TIMER", "Timer callback called, count: %" PRIu32, ++count);
+		const char *timer_name = pcTimerGetName(xTimer);
+		uint32_t timer_id = (uint32_t)pvTimerGetTimerID(xTimer);
+		ESP_LOGI("TIMER", "Timer callback called, count: %" PRIu32 ", name: %s, id: %" PRIu32, ++count, timer_name, timer_id);
 }
 
 void app_main(void)
 {
-	TimerHandle_t xTimer = xTimerCreate("Timer1",1000 / portTICK_PERIOD_MS, pdTRUE, (void *)0, timer_callback);
+	// 1000 / PortTICK_PERIOD_MS means the timer will trigger every 1000 milliseconds (1 second)
+	// 1000 / PortTICK_PERIOD_MS = pdMS_TO_TICKS(1000) = 1000 / (1000 / configTICK_RATE_HZ) = configTICK_RATE_HZ
+	TimerHandle_t xTimer1 = xTimerCreate("Timer1",1000 / portTICK_PERIOD_MS, pdTRUE, (void *)255, timer_callback);
+	TimerHandle_t xTimer2 = xTimerCreate("Timer2",pdMS_TO_TICKS(1000), pdTRUE, (void *)266, timer_callback);
 
-	xTimerStart(xTimer,0);
+	xTimerStart(xTimer1,0);
+	xTimerStart(xTimer2,0);
 
 }
