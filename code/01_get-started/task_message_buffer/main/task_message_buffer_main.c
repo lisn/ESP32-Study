25950d1f2ce5995ddef386c88601de37d197a340
shenlei <shenl@xydtek.com>
2026-03-07 17:22:33 +0800

    message buffer: message and stream Receive function different.

--- code/01_get-started/task_message_buffer/main/task_message_buffer_main.c
+++ code/01_get-started/task_message_buffer/main/task_message_buffer_main.c
@@ -13,10 +13,8 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 #include "esp_log.h"
-/*
-StreamBufferHandle_t xStreamBufferCreate( size_t xBufferSizeBytes,
-size_t xTriggerLevelBytes );
-*/
+
+#if 0
 MessageBufferHandle_t xMessageBuffer;
 
 void sendTask(void *pvParameters)
@@ -38,9 +36,10 @@ void sendTask(void *pvParameters)
 void rcvTask(void *pvParameters)
 {
 	char rx_buf[100] = {0};
-	int recvlen;
+	int recvlen; 
 	for (;;)
 	{
+		memset(rx_buf, 0, sizeof(rx_buf));
 		recvlen = xMessageBufferReceive(xMessageBuffer, rx_buf, sizeof(rx_buf), portMAX_DELAY);
 		printf("**************************************************\n");
 		printf("recv len: %d, recv data: %s\n", recvlen, rx_buf);
@@ -64,3 +63,108 @@ void app_main(void)
 		xTaskCreate(rcvTask, "rcvTask", 2048, NULL, 1, NULL);
 	}
 }
+#elif 1
+
+StreamBufferHandle_t xStreamBuffer;
+
+void sendTask(void *pvParameters)
+{
+	char tx_buf[50] = {0};
+	int strlen, sendlen;
+	for (int i = 0; i < 3; i++)
+	{
+		strlen = sprintf(tx_buf, "I am sunny,now I am in shenzhou.");
+		sendlen = xStreamBufferSend(xStreamBuffer,
+									 tx_buf,
+									 strlen,
+									 portMAX_DELAY);
+		printf("i = %d, str len: %d, send len: %d, send data: %s\n", i, strlen, sendlen, tx_buf);
+	}
+	vTaskDelete(NULL);
+}
+
+void rcvTask(void *pvParameters)
+{
+	char rx_buf[10] = {0};
+	int recvlen;
+	for (;;)
+	{
+		memset(rx_buf, 0, sizeof(rx_buf));
+		recvlen = xStreamBufferReceive(xStreamBuffer, rx_buf, sizeof(rx_buf), portMAX_DELAY);
+		printf("**************************************************\n");
+		printf("recv len: %d, recv data: %s\n", recvlen, rx_buf);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main(void)
+{
+	xStreamBuffer = xStreamBufferCreate(1000,10);
+	if (xStreamBuffer == NULL)
+	{
+		/* There was not enough heap memory space available to create the
+		stream buffer. */
+		printf("Failed to create stream buffer\n");
+	}
+	else
+	{
+		/* The stream buffer was created successfully and can now be used. */
+
+		xTaskCreate(sendTask, "sendTask", 2048, NULL, 1, NULL);
+		xTaskCreate(rcvTask, "rcvTask", 2048, NULL, 1, NULL);
+	}
+}
+
+#else
+
+
+MessageBufferHandle_t xMessageBuffer;
+
+void sendTask(void *pvParameters)
+{
+	char tx_buf[50] = {0};
+	int strlen, sendlen;
+	for (int i = 0; i < 3; i++)
+	{
+		strlen = sprintf(tx_buf, "I am sunny,now I am in shenzhou.");
+		sendlen = xMessageBufferSend(xMessageBuffer,
+									 tx_buf,
+									 strlen,
+									 portMAX_DELAY);
+		printf("i = %d, str len: %d, send len: %d, send data: %s\n", i, strlen, sendlen, tx_buf);
+	}
+	vTaskDelete(NULL);
+}
+
+void rcvTask(void *pvParameters)
+{
+	char rx_buf[10] = {0};
+	int recvlen;
+	for (;;)
+	{
+		memset(rx_buf, 0, sizeof(rx_buf));
+		recvlen = xMessageBufferReceive(xMessageBuffer, rx_buf, sizeof(rx_buf), portMAX_DELAY);
+		printf("**************************************************\n");
+		printf("recv len: %d, recv data: %s\n", recvlen, rx_buf);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main(void)
+{
+	xMessageBuffer = xMessageBufferCreate(1000);
+	if (xMessageBuffer == NULL)
+	{
+		/* There was not enough heap memory space available to create the
+		stream buffer. */
+		printf("Failed to create stream buffer\n");
+	}
+	else
+	{
+		/* The stream buffer was created successfully and can now be used. */
+
+		xTaskCreate(sendTask, "sendTask", 2048, NULL, 1, NULL);
+		xTaskCreate(rcvTask, "rcvTask", 2048, NULL, 1, NULL);
+	}
+}
+#endif
